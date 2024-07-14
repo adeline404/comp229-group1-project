46673b2137c0c341e91baa49ef606b66e537a5b1
Yik Hei Chan <adelineyikyik@gmail.com>
2024-07-14 17:46:47 -0400

    test

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -62,7 +62,7 @@ image={unicornbikeImg} title="Unicorn Bicycle"/>
 Welcome to the MERN Skeleton home page. Hello World.
 </Typography> 
 <Typography variant="body2" component="h1"> 
-Welcome!!
+Welcome!! We are Group 1.
 </Typography> 
 </CardContent>
 </Card> 
