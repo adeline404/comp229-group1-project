7cfafd2b3ffb004f260e27be5ee2fee000249191
Yik Hei Chan <adelineyikyik@gmail.com>
2024-07-15 16:31:26 -0400

    fix deploy problem

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
-import config from './config/config.js'
-import app from './server/express.js'
-import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
+// import config from './config/config.js'
+// import app from './server/express.js'
+// import mongoose from 'mongoose'
+// mongoose.Promise = global.Promise
+// mongoose.connect(config.mongoUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+
+// mongoose.connection.on('error', () => {
+//   throw new Error(`unable to connect to database: ${config.mongoUri}`)
+// })
+
+// app.listen(config.port, (err) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.info('Server started on port %s.', config.port)
+// })
+
+import config from './config/config.js';
+import app from './server/express.js';
+import mongoose from 'mongoose';
+import path from 'path';
+import express from 'express'; // Import the express module
+
+const __dirname = path.resolve();
+
+mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+.then(() => {
+  console.log("Connected to the database!");
 })
+.catch((error) => {
+  console.error("Error connecting to database:", error);
+  process.exit(1); // Exit process on database connection error
+});
 
-app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
-})
+app.use(express.static(path.join(__dirname, 'client')));
 
+// Route handler for root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+const PORT = config.port || 3000;
+app.listen(PORT, () => {
+  console.info(`Server started on port ${PORT}`);
+});
--- server/express.js
+++ server/express.js
@@ -1,28 +1,65 @@
 
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
+// import compress from "compression";
+// import cors from "cors";
+// import helmet from "helmet";
+// import Template from "./../template.js";
+// import userRoutes from "./routes/user.routes.js";
+// import authRoutes from "./routes/auth.routes.js";
+// import path from "path";
+
+// const app = express();
+// const CURRENT_WORKING_DIR = process.cwd();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("./server/dist/app"));
+// app.use("/", userRoutes);
+// app.use("/", authRoutes);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(compress());
+// app.use(helmet());
+// app.use(cors());
+// app.use((err, req, res, next) => {
+//   if (err.name === "UnauthorizedError") {
+//     res.status(401).json({ error: err.name + ": " + err.message });
+//   } else if (err) {
+//     res.status(400).json({ error: err.name + ": " + err.message });
+//     console.log(err);
+//   }
+// });
+// export default app;
 import express from "express";
-import bodyParser from "body-parser";
+import path from "path";
 import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
-import Template from "./../template.js";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
-import path from "path";
 
 const app = express();
-const CURRENT_WORKING_DIR = process.cwd();
+const __dirname = path.resolve();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./server/dist/app"));
-app.use("/", userRoutes);
-app.use("/", authRoutes);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+
+// Serve static files from the 'client' directory
+app.use(express.static(path.join(__dirname, 'server/dist/app')));
+
+// Routes
+app.use("/", userRoutes);
+app.use("/", authRoutes);
+
+// Error handling middleware
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
@@ -31,4 +68,5 @@ app.use((err, req, res, next) => {
     console.log(err);
   }
 });
+
 export default app;
